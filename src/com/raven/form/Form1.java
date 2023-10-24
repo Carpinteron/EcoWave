944e26377010e346d7697f431e87c9f5865e053d
Ari <isabellajuliao@uninorte.edu.co>
2023-10-24 04:36:11 -0500

    LINEA EN AZUL

--- src/com/raven/form/Form1.java
+++ src/com/raven/form/Form1.java
@@ -113,7 +113,7 @@ public class Form1 extends javax.swing.JPanel {
         panel.setPreferredSize(new java.awt.Dimension(560, 367));
         XYPlot plot = chart.getXYPlot();
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesPaint(0, Color.BLUE);
         renderer.setSeriesStroke(0, new BasicStroke(2.0f));
         plot.setRenderer(renderer);
         jPanel1.setLayout(null);
